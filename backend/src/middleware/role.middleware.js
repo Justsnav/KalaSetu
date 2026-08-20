8bc744b2cc4c9e19cf107a213446fa2b2a934b4b
Justsnav <abhinavsingh882005@email.com>
2026-08-21 03:05:23 +0530

    feat: add product CRUD operations

--- backend/src/middleware/role.middleware.js
+++ backend/src/middleware/role.middleware.js
@@ -5,7 +5,7 @@ const restrictTo = (...allowedRoles)=>{
                 message : 'You do not have permission to perform this action'
             });
         }
-        next;
+        next();
     }
 }
 
