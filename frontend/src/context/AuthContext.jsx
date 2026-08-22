b01fab42e07650fb43c91c6908edd413729f1b34
Justsnav <abhinavsingh882005@gmail.com>
2026-08-22 23:29:21 +0530

    Frontend- AuthContext

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,10 @@
+//context is a new react concept that lets multiple components(navbar, login page, protected pages) all know "who logged in "
+
+import { useState, createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({children}){
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem('token'));
+}
